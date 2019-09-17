da430a084fe1e29124d6f95a74d77b2b94ccb355
SantiagoIlli <santiagoilli96@gmail.com>
2019-09-17 15:31:31 -0300

    Minimal reproduction ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NgZone } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import {LazyElementsModule} from '@angular-extensions/elements';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, LazyElementsModule ],
   declarations: [ AppComponent, HelloComponent ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
   bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private zone: NgZone) {
+    (window as any).ngZone = this.zone;
+  }
+}
